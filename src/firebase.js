0545ce5ac12b8b1e1bbf2dd1c6cd7275c2f950d4
ArroVX <arroudhilanfi01@gmail.com>
2026-02-01 01:07:09 +0700

    Config: Setup production Firebase credentials for Guestbook

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // 4. Copy bagian "firebaseConfig" dan ganti di bawah ini:
 
 const firebaseConfig = {
-    apiKey: "TUANGKAN_API_KEY_KAMU_DISINI",
-    authDomain: "PROJECT_KAMU.firebaseapp.com",
-    projectId: "PROJECT_KAMU",
-    storageBucket: "PROJECT_KAMU.appspot.com",
-    messagingSenderId: "SENDER_ID_KAMU",
-    appId: "APP_ID_KAMU"
+    apiKey: "AIzaSyCR78HlkHS4qwSrgPIhTguCYzgjckLNu0Q",
+    authDomain: "arro-portofolio.firebaseapp.com",
+    projectId: "arro-portofolio",
+    storageBucket: "arro-portofolio.firebasestorage.app",
+    messagingSenderId: "618950653172",
+    appId: "1:618950653172:web:7a0e8d8e70099d20a1daa1",
+    measurementId: "G-4J9X1JW0F9"
 };
 
 // Hubungkan ke Firebase
